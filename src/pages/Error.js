0593a29955fda4235358746db29782780ba76c6d
Mandy Torrence <mandolinhaynes@gmail.com>
2020-03-30 00:22:06 -0400

    Final Tweaks

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -9,6 +9,11 @@ const Error = () => {
             <div className="oops-content">
                 <h1>Oops!</h1>
                 <h3>No page was found, but don't get mad, just visit one of the pages below:</h3>
+                <div className="page-buttons">
+                    <a href="/" target="_blank"><button>About</button></a>
+                    <a href="/portfolio" target="_blank"><button>Portfolio</button></a>
+                    <a href="/contact" target="_blank"><button>Contact</button></a>
+                </div>
             </div>
         </div>
 
